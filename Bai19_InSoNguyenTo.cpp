7ea1f59874189b5397d61342bbe8f4ef2c7dfdc8
nguyenhoangkhoi06011 <nguyenhoangkhoi012021@gmail.com>
2024-11-25 15:35:53 +0700

    inSoNguyenTo

--- Bai19_InSoNguyenTo.cpp
+++ Bai19_InSoNguyenTo.cpp
@@ -1,36 +1,70 @@
 #include <iostream>
 using namespace std;
-int main() {
-	int n;
+
+
+bool(kiemtraSNT(int s)) {
 	int dem = 0;
-	cout << "nhap so:\n"; cin >> n;
-	for (int m = 1; y <= m; m++) {
-		dem = 0;
-		for (int i = 1; i <= m; i++)
-		{
-			if (m % i == 0) {
-				dem++;
-
-			}
-		}
-		if (dem == 2) {
-			cout << y << " ";
-		}
-	}/*
-	for (int i = 1; i <= n; i++)
+	for (int i = 1; i <= s; i++)
 	{
-		if (n % i == 0) {
+		if (s % i == 0)
+		{
 			dem++;
-
 		}
 	}
-	//cout << "so dem =" << dem << endl;
-	if (dem == 2) {
-		cout << "So " << n << " la so nguyen to\n";
+	if (dem == 2)
+	{
+		return true;
 	}
-	else{
-		cout << "So " << n << " khong phai la so nguyen to\n";
-	}*/
-	return 0;
+	else {
+		return false;
+	}
+}
 
+
+bool kiemtraSCP(int x) {
+	if (sqrt(x) == (int)sqrt(x)) return true;
+	return false;
+}
+
+
+int main()
+{
+	cout << "Kiem tra so co phai la so nguyen to hay khong\n";
+	int so;
+	cout << "Nhap so: "; cin >> so;
+
+	for (int m = 1; m <= so; m++)
+	{
+		if (kiemtraSNT(m) == true) cout << m << " ";
+	}
+
+	cout << endl;
+
+
+	for (int i = 1; i < 100; i++)
+	{
+		if (kiemtraSCP(i) == true) cout << i << " ";
+	}
+
+
+	/*
+	for (int i = 1; i <= so; i++)
+	{
+		if (so %i == 0)
+		{
+			dem++;
+		}
+	}
+	cout << "So dem = " << dem << endl;
+	if (dem == 2)
+	{
+		cout << "So " << so << " La so nguyen to\n";
+	}
+	else
+	{
+		cout << "So " << so << " Khong phai la so nguyen to\n";
+	}
+	*/
+
+	return 0;
 }
